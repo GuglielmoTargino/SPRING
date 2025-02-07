6759a60cd57a3dbdd22917caa4b63de359c62fc8
Guglielmo Henriques Targino <guglielmohenriquestargino@gmail.com>
2025-02-07 12:01:19 -0300

    icini
    Iniciado o curso de springboot

---
+++ pjtfullstack/src/main/java/com/cursospringboot/pjtfullstack/PjtfullstackApplication.java
@@ -0,0 +1,13 @@
+package com.cursospringboot.pjtfullstack;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PjtfullstackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PjtfullstackApplication.class, args);
+	}
+
+}
---
+++ pjtfullstack/src/test/java/com/cursospringboot/pjtfullstack/PjtfullstackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cursospringboot.pjtfullstack;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PjtfullstackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

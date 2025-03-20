228386cf610628f16fd133e8ea26336fba88e493
Guglielmo Henriques Targino <guglielmohenriquestargino@gmail.com>
2025-03-20 18:52:09 -0300

    atz
    atualização apenas

--- calculo/src/main/java/com/calculos/calculo/controller/CalculosController.java
+++ calculo/src/main/java/com/calculos/calculo/controller/CalculosController.java
@@ -4,8 +4,11 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping
+@RequestMapping("/calculos")
 
 public class CalculosController {
+    public ResponseEntity<Resultado> soma(Entrada entrada){
+        
+    }
 
 }

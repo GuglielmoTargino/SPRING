2724f5614ded17d9e38999ceef56624fe50fe503
Guglielmo Henriques Targino <guglielmohenriquestargino@gmail.com>
2025-04-04 11:55:37 -0300

    ini_save_carro
    Iniciado crud salvar.

---
+++ calculo/src/main/java/com/calculos/calculo/repository/CarroRepository.java
@@ -0,0 +1,8 @@
+package com.calculos.calculo.repository;
+import com.calculos.calculo.entity.Carro;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CarroRepository extends JpaRepository<Carro,Long>{
+
+}
--- calculo/src/main/java/com/calculos/calculo/service/CarroService.java
+++ calculo/src/main/java/com/calculos/calculo/service/CarroService.java
@@ -1,12 +1,19 @@
 package com.calculos.calculo.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import com.calculos.calculo.entity.Carro;
+import com.calculos.calculo.repository.CarroRepository;
 
 
 @Service
 public class CarroService {
+    @Autowired
+    private CarroRepository carroRepository;
+
     public String save(Carro carro){
+
+        this.carroRepository.save(carro);
         // Query aqui
         return "Carro cadastrado";
     }

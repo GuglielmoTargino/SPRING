d2940b0ef737e9e4fe9583615d9b0dff1e3b942a
Guglielmo Henriques Targino <guglielmohenriquestargino@gmail.com>
2025-04-07 10:46:39 -0300

    ini_crud_simples
    Iniciado o crud simples.

---
+++ crudsimplessb/src/main/java/com/crudsimples/crudsimplessb/CrudsimplessbApplication.java
@@ -0,0 +1,13 @@
+package com.crudsimples.crudsimplessb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrudsimplessbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrudsimplessbApplication.class, args);
+	}
+
+}
---
+++ crudsimplessb/src/test/java/com/crudsimples/crudsimplessb/CrudsimplessbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.crudsimples.crudsimplessb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrudsimplessbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

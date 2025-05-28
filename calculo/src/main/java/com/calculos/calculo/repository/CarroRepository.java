c75466222b55cd1e61caf46b86e4523f4d9389db
Guglielmo Henriques Targino <guglielmohenriquestargino@gmail.com>
2025-05-28 00:18:35 -0300

    ini_crud_oracle
    Iniciado o crud com oracle

--- calculo/src/main/java/com/calculos/calculo/repository/CarroRepository.java
+++ calculo/src/main/java/com/calculos/calculo/repository/CarroRepository.java
@@ -1,7 +1,8 @@
 package com.calculos.calculo.repository;
-import com.calculos.calculo.entity.Carro;
 
-import org.springframework.data.jpa.repository.JpaRepository;//Caarrro Repositorio.
+
+import com.calculos.calculo.entity.Carro;
+import org.springframework.data.jpa.repository.JpaRepository;//Caarrro Repositorio JPA serve com qualquer banco..
 
 public interface CarroRepository extends JpaRepository<Carro,Long>{
 

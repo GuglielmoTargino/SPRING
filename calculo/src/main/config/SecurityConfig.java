b562f1885c159ce4710aeb4daf0d57a6c2d71094
Guglielmo Henriques Targino <guglielmohenriquestargino@gmail.com>
2025-07-17 23:32:46 -0300

    addj
    Ajuste de endpoint

--- calculo/src/main/config/SecurityConfig.java
+++ calculo/src/main/config/SecurityConfig.java
@@ -33,7 +33,8 @@ public class SecurityConfig  {
 		.csrf(AbstractHttpConfigurer::disable)
 		.cors(AbstractHttpConfigurer::disable)
 		.authorizeHttpRequests((requests) -> requests
-				.requestMatchers("/api/login").permitAll() //seta como publico
+				.requestMatchers("admin/carros").permitAll() //seta como publico
+				//.requestMatchers("/api/login").permitAll() //seta como publico
 				//.requestMatchers("/api/register").permitAll()// para registrar usuarios caso tenha tela
 				.anyRequest().authenticated())
 		.authenticationProvider(authenticationProvider)

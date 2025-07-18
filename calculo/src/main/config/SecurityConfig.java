f51d943fe897279c693f836a897aea046e4ff895
Guglielmo Henriques Targino <guglielmohenriquestargino@gmail.com>
2025-07-17 22:29:49 -0300

    add_sec
    Adição security

--- calculo/src/main/config/SecurityConfig.java
+++ calculo/src/main/config/SecurityConfig.java
@@ -33,8 +33,8 @@ public class SecurityConfig  {
 		.csrf(AbstractHttpConfigurer::disable)
 		.cors(AbstractHttpConfigurer::disable)
 		.authorizeHttpRequests((requests) -> requests
-				.requestMatchers("/api/login").permitAll()
-				.requestMatchers("/api/register").permitAll()
+				.requestMatchers("/api/login").permitAll() //seta como publico
+				//.requestMatchers("/api/register").permitAll()// para registrar usuarios caso tenha tela
 				.anyRequest().authenticated())
 		.authenticationProvider(authenticationProvider)
 		.addFilterBefore(jwtAuthFilter, UsernamePasswordAuthenticationFilter.class)

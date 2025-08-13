f697a6534f9cb73d54c20bf9c56e63a90b80c038
Guglielmo Henriques Targino <guglielmohenriquestargino@gmail.com>
2025-08-12 23:18:01 -0300

    outro_teste
    Finalizado outro teste com netbens para app

---
+++ pjto2mvn/src/main/java/com/testesgugas/pjto2mvn/Pjto2mvn.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.testesgugas.pjto2mvn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ *
+ * @author Guglielmo H T
+ */
+
+@SpringBootApplication
+public class Pjto2mvn {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Pjto2mvn.class, args);
+        System.out.println("Hello World!");
+    }
+}
--- pjto2mvn/src/main/java/com/testesgugas/pjto2mvn/controller/Hello2.java
+++ pjto2mvn/src/main/java/com/testesgugas/pjto2mvn/controller/Hello2.java
@@ -2,7 +2,7 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-package com.netbeansteste1api.mavenproject1netbeans.controler;
+package com.testesgugas.pjto2mvn.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -12,14 +12,13 @@ import org.springframework.web.bind.annotation.RestController;
  *
  * @author Guglielmo H T
  */
-
 @RestController
-@RequestMapping("/api")
-public class Hello {
+@RequestMapping("/ppi")
+public class Hello2 {
     
-    @GetMapping("/hello")    
-    public String heloo(){
-        return "HELLO FUTURE!";
+    @GetMapping("/hello")
+    public String hello() {
+        return "Hello GUGA!";
     }
     
 }
